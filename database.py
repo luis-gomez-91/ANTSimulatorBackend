b2216e2e95ce334135b7d1fac6c81b8a844b0ea1
luisGomez <luis.gomez.veloz@gmail.com>
2025-04-28 17:23:43 -0500

    Requriments

--- database.py
+++ database.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-URL_DATABASE = 'postgresql://postgres:luis@localhost:5432/ant'
+# URL_DATABASE = 'postgresql://postgres:luis@localhost:5432/ant'
+URL_DATABASE = 'postgresql://ant_nf5j_user:Uu2c5w5a5sKp39IpiYjIJyid3nOPV4zf@dpg-d07vrus9c44c73bcjak0-a/ant_nf5j'
 engine = create_engine(URL_DATABASE)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
