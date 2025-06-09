b4023d628e9303d6a909926993f453c8f3e43fc9
luisGomez <luis.gomez.veloz@gmail.com>
2025-06-09 15:12:16 -0500

    prueba

--- main.py
+++ main.py
@@ -21,7 +21,6 @@ origins = [
     "http://127.0.0.1:8000",
 ]
 
-# app.add_middleware(HTTPSRedirectMiddleware)
 if os.getenv("RAILWAY_ENVIRONMENT") != "production":
     app.add_middleware(HTTPSRedirectMiddleware)
 
@@ -32,7 +31,7 @@ app.add_middleware(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
