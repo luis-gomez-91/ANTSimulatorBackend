f17804ce59828ba066c4ff40f204dbf0868787f3
luisGomez <luis.gomez.veloz@gmail.com>
2025-06-09 13:31:38 -0500

    allow_origins *

--- main.py
+++ main.py
@@ -30,10 +30,10 @@ app.add_middleware(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"]
+    allow_headers=["*"],
 )
 
 load_dotenv()
